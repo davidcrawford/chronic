fa120ace052efbcf2d71315ab421769c3df89330
David Crawford <david@minetowin.com>
2012-12-10 12:13:55 -0800

    Fix distribution

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 
 setup(name='chronic',
-      version='0.1',
+      version='0.1.2',
       py_modules=['chronic'],
+      packages=['signals'],
       description='Half profiler, half timer, 100% gangsta',
       url='http://github.com/davidcrawford/chronic',
       author='David Crawford',
-      author_email='david.crawford@gmail.com'
+      author_email='david.crawford@gmail.com',
       license='MIT'
       )
