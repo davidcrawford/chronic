6e87eff61cd65099f6133ebb0e3c34d57d351e40
David Crawford <david@minetowin.com>
2012-12-10 11:55:41 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='chronic',
+      version='0.1',
+      py_modules=['chronic'],
+      description='Half profiler, half timer, 100% gangsta',
+      url='http://github.com/davidcrawford/chronic',
+      author='David Crawford',
+      author_email='david.crawford@gmail.com'
+      )
